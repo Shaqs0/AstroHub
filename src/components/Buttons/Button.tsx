09dc1bb7b1a3b33cad8c30856141fed7660f937a
Shaqs0 <hhgfhhhggb@gmail.com>
2024-03-06 19:45:47 +0300

    /

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -7,6 +7,7 @@ function Button({ children, className, appearence = 'small', ...props }: ButtonP
 	return (
 		<button className={cn(styles['button'], styles['accent'], className, {
 			[styles['small']]: appearence === 'small',
+			[styles['middle']]: appearence === 'middle',
 			[styles['big']]: appearence === 'big'
 		})} {...props}>{children}</button>
 	);
--- src/layout/MainPage/Layout.tsx
+++ src/layout/MainPage/Layout.tsx
@@ -1,8 +1,16 @@
-import Button from '../../components/Buttons/Button';
+import Search from '../../components/Search/Search';
 import styles from './Layout.module.css';
 
 export function Layout() {
 	return <div className={styles['layout']}>
-		<Button>s</Button>
+		<div className={styles['top']}>
+			<div className={styles['logo']}>
+				<img src='../public/AstroHub_logo.svg' alt='Логотип сайта' />
+				<p>AstroHub</p>
+			</div>
+			<div className={styles['search']}><Search /></div>
+			<div className={styles['profile-area']}></div>
+			
+		</div>
 	</div>;
 }
